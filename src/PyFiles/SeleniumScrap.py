fe50e48a45c7d61cdb6c96bea1da627174241ed4
kristianbraathen <97292975+kristianbraathen@users.noreply.github.com>
2025-01-20 10:46:47 +0100

    Update SeleniumScrap.py

--- src/PyFiles/SeleniumScrap.py
+++ src/PyFiles/SeleniumScrap.py
@@ -10,7 +10,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import re
 import time
-import gradio as gr
 from flask import Flask, jsonify, Blueprint, request
 
 api4_blueprint = Blueprint('api4', __name__)
@@ -19,7 +18,7 @@ options = Options()
 options.binary_location = r'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
 chrome_service = Service('C:\\Users\\Krist\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe')
 
-# Sett opp Chrome med nødvendige innstillinger
+# Sett opp Chrome med nÃ¸dvendige innstillinger
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
@@ -37,32 +36,32 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 
 
 
-# options.add_argument('--headless')  # Fjern for å teste med GUI
+# options.add_argument('--headless')  # Fjern for Ã¥ teste med GUI
 
 driver = webdriver.Chrome(service=chrome_service, options=options)
 
 def find_emails_on_facebook(company_name):
     try:
-        # Google-søk etter firma på Facebook
+        # Google-sÃ¸k etter firma pÃ¥ Facebook
         search_url = f"https://www.google.com/search?q=site:facebook.com+OR+site:1881.no+{company_name}&hl=no&gl=no&cr=countryNO"
         
         driver.get(search_url)
-        # Vent på at søkeresultater skal laste inn
+        # Vent pÃ¥ at sÃ¸keresultater skal laste inn
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH,"//h3"))
         )
         
-        # Finn det første søkeresultatet
+        # Finn det fÃ¸rste sÃ¸keresultatet
         first_result = driver.find_element(By.XPATH, "//h3")
         
-        # Forsøk å klikke med ulike strategier
+        # ForsÃ¸k Ã¥ klikke med ulike strategier
         try:
             # Rull til elementet
             driver.execute_script("arguments[0].scrollIntoView(true);", first_result)
             # Klikk med JavaScript som en sikkerhetskopi
             driver.execute_script("arguments[0].click();", first_result)
         except:
-            # Vent på at blokkerende element skal forsvinne
+            # Vent pÃ¥ at blokkerende element skal forsvinne
             WebDriverWait(driver, 10).until(
                 EC.invisibility_of_element((By.CLASS_NAME, "jw8mI"))
             )
@@ -74,14 +73,14 @@ def find_emails_on_facebook(company_name):
         # Vent til siden laster inn
         time.sleep(5)
 
-        # Få sidekilden og parse den
+        # FÃ¥ sidekilden og parse den
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
 
         # Finn e-postadresser med BeautifulSoup
         emails = set()
         for text in soup.stripped_strings:
-            # Bruk regex for å finne e-postadresser i teksten
+            # Bruk regex for Ã¥ finne e-postadresser i teksten
             found_emails = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", text)
             emails.update(found_emails)
         
