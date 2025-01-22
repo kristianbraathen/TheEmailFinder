bb665f41b3d981a6af8f88903de5f382bb53d5e0
kristianbraathen <97292975+kristianbraathen@users.noreply.github.com>
2025-01-22 11:23:09 +0100

    Update KseApi.py

--- src/PyFiles/KseApi.py
+++ src/PyFiles/KseApi.py
@@ -16,7 +16,7 @@ api3_blueprint = Blueprint('api3', __name__)
 process_lock = Lock()
 process_running = False  # Global flag to track the process state 
 
-os.environ["PATH"] += os.pathsep + "/usr/bin/google-chrome"
+os.environ["CUSTOM_PATH"] += os.pathsep + "/usr/bin/google-chrome"
 chromedriver_autoinstaller.install()  
 # Konfigurasjon for Selenium
 chrome_service = Service()
