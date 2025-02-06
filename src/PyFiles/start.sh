a4dd45397c7c7640d086299adbf1ba5a60380ca1
kristianbraathen <97292975+kristianbraathen@users.noreply.github.com>
2025-02-06 11:06:24 +0100

    Update start.sh

--- src/PyFiles/start.sh
+++ src/PyFiles/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
-# Start the frontend or another server
-npm run server &
-# Start the Flask app
+
+# Bygg frontend
+cd /app/frontend
+npm install
+npm run build
+
+# Flytt bygde filer til backend for å serve dem med Flask
+mv dist /app/backend/static
+
+# Start backend (Gunicorn)
+cd /app/backend
 gunicorn --bind 0.0.0.0:8080 app:app
